6687930541778e5565dfc72d8b95fc50517b9e82
codexlynx <codexlynx@gmail.com>
2019-12-03 16:24:45 +0000

    feat(logs): Add logs command and code refactor

--- __init__.py
+++ __init__.py
@@ -5,32 +5,57 @@ import docker
 
 class DockerDaemonSkill(Skill):
 
+    def blacklist(self):
+        blacklist = self.config.get('containers-blacklist', [])
+        if blacklist is None:
+            return []
+        return blacklist
+
+    def docker_client(self):
+        return docker.DockerClient(base_url=self.config['daemon-url'])
+
+    def list_containers(self):
+        client = self.docker_client()
+        for container in client.containers.list():
+            if container.name not in self.blacklist():
+                yield container
+
+    def get_container(self, message):
+        client = docker.DockerClient(base_url=self.config['daemon-url'])
+        container_id = message.parse_result['container_id']
+        container = client.containers.get(container_id)
+        return container
+
     @match_parse('/docker_ps')
     async def ps(self, message):
-        client = docker.DockerClient(base_url=self.config['daemon-url'])
-        for container in client.containers.list():
-            if container.name not in self.config['containers-blacklist']:
-                msg = 'Container ID: %s\nImage: %s\nNames: %s\n\n/docker_restart_%s\n/docker_stop_%s' % (
-                    container.short_id,
-                    container.image.tags[0],
-                    container.name,
-                    container.short_id,
-                    container.short_id
-                )
-                await message.respond(msg)
+        for container in self.list_containers():
+            msg = 'Container ID: %s\nImage: %s\nNames: %s\n\n/docker_logs@%s\n' \
+                  '/docker_restart@%s\n/docker_stop@%s' % (
+                container.short_id,
+                container.image.tags[0],
+                container.name,
+                container.short_id,
+                container.short_id,
+                container.short_id
+            )
+            await message.respond(msg)
 
-    @match_parse('/docker_restart_{container_id}')
+    @match_parse('/docker_restart@{container_id}')
     async def restart(self, message):
-        client = docker.DockerClient(base_url=self.config['daemon-url'])
-        container_id = message.parse_result['container_id']
-        container = client.containers.get(container_id)
+        container = self.get_container(message)
         container.restart()
-        await message.respond('Container: %s restarted!' % container_id)
+        await message.respond('Container: %s restarted!' % container.short_id)
 
-    @match_parse('/docker_stop_{container_id}')
+    @match_parse('/docker_stop@{container_id}')
     async def stop(self, message):
-        client = docker.DockerClient(base_url=self.config['daemon-url'])
-        container_id = message.parse_result['container_id']
-        container = client.containers.get(container_id)
+        container = self.get_container(message)
         container.stop()
-        await message.respond('Container: %s stopped!' % container_id)
+        await message.respond('Container: %s stopped!' % container.short_id)
+
+    @match_parse('/docker_logs@{container_id}')
+    async def logs(self, message):
+        container = self.get_container(message)
+        for line in container.logs().decode('utf8').splitlines():
+            await message.respond(line)
+
+
